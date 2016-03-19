bdf09f9396f56076d7d79bdff7d5a5d96046fbfe
Thomas Garrett <thomasg31193@gmail.com>
2016-03-19 15:22:46 -0400

    Added files via upload

---
+++ riseandsetprogram.php
@@ -0,0 +1,15 @@
+<?php 
+
+	// Filename: riseandsetprogram.php
+	// Programmer: Thomas Garrett
+	// Purpose: Create and display a program to show the sunrise and sunset date
+	
+	echo "EST Sunrise and Date - " . "<br>";
+
+	print date("D M d Y"). ', sunrise time: ' .date_sunrise(time(), SUNFUNCS_RET_STRING, 33, -80, 90, -5) . "<br>" . "<br>";
+	
+	echo "EST Sunset and Date - " . "<br>";	
+	
+	print date("D M d Y"). ', sunset time: ' .date_sunset(time(), SUNFUNCS_RET_STRING, 33, -80, 90, -5);
+	
+?>
